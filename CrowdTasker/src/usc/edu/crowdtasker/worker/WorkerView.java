6c9f41f1274a173ff79ddd1ccb7c851d0508b7fe
Aditya Dhulipala <Aditya Dhulipala@ADStation>
2014-11-20 13:02:53 -0800

    almost done with task2).
    need to fix draw-on-refresh

--- CrowdTasker/src/usc/edu/crowdtasker/worker/WorkerView.java
+++ CrowdTasker/src/usc/edu/crowdtasker/worker/WorkerView.java
@@ -1,8 +1,20 @@
 package usc.edu.crowdtasker.worker;
 
+import java.io.IOException;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import usc.edu.crowdtasker.R;
 import usc.edu.crowdtasker.UpdatableFragment;
@@ -12,19 +24,28 @@ import usc.edu.crowdtasker.data.provider.TaskProvider;
 import usc.edu.crowdtasker.data.provider.UserProvider;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView.FindListener;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -37,15 +58,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 
 public class WorkerView extends Fragment implements LocationListener, 
-		OnMarkerClickListener, OnMapClickListener, UpdatableFragment{
+		OnMarkerClickListener, OnMapClickListener, UpdatableFragment, OnClickListener {
 
 	public static final String TAG = "WorkerView";
 	
     private GoogleMap mMap;
     private FrameLayout mapWrapper;
+    private View rootView;
     private LocationManager locationManager;
 
     private static final long MIN_TIME = 400;
@@ -82,6 +106,7 @@ public class WorkerView extends Fragment implements LocationListener,
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.worker_view, container, false);
+        this.rootView = rootView;
         mapWrapper = (FrameLayout)rootView.findViewById(R.id.map_wrapper);
         mapWrapper.setVisibility(View.INVISIBLE);
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
@@ -135,6 +160,7 @@ public class WorkerView extends Fragment implements LocationListener,
     	mMap.setMyLocationEnabled(true);
     	mMap.setOnMapClickListener(this);
     	mMap.setOnMarkerClickListener(this);
+    	mMap.getUiSettings().setZoomControlsEnabled(false);
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(
         		LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this); 
@@ -211,6 +237,8 @@ public class WorkerView extends Fragment implements LocationListener,
 
 	@Override
 	public boolean onMarkerClick(Marker marker) {
+		
+		
 		if(openDropoffMarker != null){
 			if(openDropoffMarker.equals(marker)){
 				if(openPickupMarker != null)
@@ -232,6 +260,7 @@ public class WorkerView extends Fragment implements LocationListener,
 
 				openDropoffMarker = mMap.addMarker(opt);
 				openDropoffMarker.showInfoWindow();
+				showTaskDetailsPanel(task);
 			}
 			openPickupMarker = marker;
 			openPickupMarker.showInfoWindow();
@@ -240,6 +269,96 @@ public class WorkerView extends Fragment implements LocationListener,
 		return true;
 	}
 	
+	private void showTaskDetailsPanel(Task task) {
+		this.task = task;
+		final LinearLayout taskPanel = (LinearLayout) rootView.findViewById(R.id.task_details_panel);
+		String directions;
+		taskPanel.setVisibility(View.VISIBLE);
+		
+		TextView taskheading = (TextView) mapWrapper.findViewById(R.id.taskHeading);
+		TextView taskDetails = (TextView) mapWrapper.findViewById(R.id.taskDetails);
+		
+		taskheading.setText(task.getName());
+		taskDetails.setText(task.getDescription());
+		
+		Button acceptBtn = (Button) mapWrapper.findViewById(R.id.accpetTaskBtn);
+		Button declineBtn = (Button) mapWrapper.findViewById(R.id.declineTaskBtn);
+		
+		acceptBtn.setOnClickListener(this);
+		declineBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				taskPanel.setVisibility(View.GONE);
+				
+			}
+		});
+		
+	}
+	
+	private PolylineOptions POLYLINES;
+
+	private void updateMap(String result2) {
+		try {
+			JSONObject json = new JSONObject(result2);
+			JSONArray routeArray = json.getJSONArray("routes");
+			
+			JSONObject routes = routeArray.getJSONObject(0);
+			JSONObject polylines = routes.getJSONObject("overview_polyline");
+			
+            String encodedString = polylines.getString("points");
+            List<LatLng> list = decodePoly(encodedString);
+            
+            PolylineOptions options = new PolylineOptions().width(5).color(Color.BLUE).geodesic(true);
+            for (int z = 0; z < list.size(); z++) {
+                LatLng point = list.get(z);
+                options.add(point);
+            }
+            POLYLINES = options;
+            mMap.addPolyline(options);
+            		
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	private List<LatLng> decodePoly(String encoded) {
+
+		List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+ 
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+ 
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+ 
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                        (((double) lng / 1E5)));
+            poly.add(p);
+        }
+ 
+        return poly;
+	}
+
 	private void fitToOpenMarkers(){
 		if(openPickupMarker == null)
 			return;
@@ -269,22 +388,56 @@ public class WorkerView extends Fragment implements LocationListener,
 	}
     
 
-	@Override
-	public void onProviderDisabled(String provider) {		
-	}
 
+	private Task task;
+	private String result = "";  
+	private String directions;
 	@Override
-	public void onProviderEnabled(String provider) {		
-	}
+	public void onClick(View v) {
+		String pickUp = task.getPickupAddress();
+		String dropOff = task.getDropoffAddress();
 
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {		
+		
+		String url = "https://maps.googleapis.com/maps/api/directions/json?"
+				+ "origin=" + Uri.encode(pickUp) +"&"
+				+"destination="+ Uri.encode(dropOff ) + "&"
+				+ "mode=walking"; 
+		Log.d("RESTCALL",url);
+		
+		callWebService(url); 
+		this.directions = result;
+		updateMap(result);
 	}
+	 public void callWebService(String url){  
+	        HttpClient httpclient = new DefaultHttpClient();  
+	        HttpGet request = new HttpGet(url);  
+	        ResponseHandler<String> handler = new BasicResponseHandler();  
+	        try {  
+	            result = httpclient.execute(request, handler);  
+	        } catch (ClientProtocolException e) {  
+	            e.printStackTrace();  
+	        } catch (IOException e) {  
+	            e.printStackTrace();  
+	        }  
+	        httpclient.getConnectionManager().shutdown();   
+	        Log.i("RESTCALL", result);  
+	    } // end callWebService()  
+		@Override
+		public void onProviderDisabled(String provider) {		
+		}
 
-	@Override
-	public void update() {
-		currentUser = UserProvider.getCurrentUser(getActivity());
-        showTasksOnMap(currentLocation);
-	}
+		@Override
+		public void onProviderEnabled(String provider) {		
+		}
 
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {		
+		}
+
+		@Override
+		public void update() {
+			currentUser = UserProvider.getCurrentUser(getActivity());
+	        showTasksOnMap(currentLocation);
+		}
 }
+
